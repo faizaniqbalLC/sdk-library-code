988d658eeae2d9e7f24ccb10c1eca8de4593f2cf
fizan-iqbal <se.fizaniqbal2@gmail.com>
2023-03-03 19:37:57 +0500

    npmLibrary

---
+++ sdk-test/src/app.ts
@@ -0,0 +1,11 @@
+import Typicode from "sdknow";
+const client = new Typicode({
+  apiKey: "123",
+});
+
+// client.getPosts().then((res) => {
+//   console.log(res);
+// });
+client.getPostById(1).then((res) => {
+  console.log(res);
+});
